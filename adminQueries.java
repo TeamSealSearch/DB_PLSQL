ca117c953cce4ab0c3616a6950b1f3d3de4a95ee
JGONZA90 <sledge517@gmail.com>
2021-05-24 10:53:08 -0500

    Update Admin Queries

--- adminQueries.java
+++ adminQueries.java
@@ -9,16 +9,16 @@ String stringWithPlaceHolder = "test String with placeholders {0} {1} {2} {3}";
 String formattedString = java.text.MessageFormat.format(stringWithPlaceHolder, "place-holder-1", "place-holder-2", "place-holder-3", "place-holder-4");
 */
 
-static final String createAdmin = "INSERT INTO ADMINISTRATOR (admin_hashedid, admin_username, admin_fname, admin_lname, admin_dob) VALUES ('9z$Cw)J@NcRfUjX', 'Seal Search Admin', 'Sammy', 'Seal', SYSDATE)"; 
+static final String createAdmin = "INSERT INTO ADMINISTRATOR (admin_hashedid, admin_username, admin_fname, admin_lname, admin_dob) VALUES (\'9z$Cw)J@NcRfUjX\', \'Seal Search Admin\', \'Sammy\', \'Seal\', \'2021-01-29\')"; 
 
-static final String updateAdminInfo = "UPDATE ADMINISTRATOR SET admin_username = 'The Creator', admin_fname = 'Spaghetti', admin_lname = 'Monster', admin_dob = SYSDATE WHERE admin_hashedid = '9z$Cw)J@NcRfUjX';";
+static final String updateAdminInfo = "UPDATE ADMINISTRATOR SET admin_username = \'The Creator\', admin_fname = \'Spaghetti\', admin_lname = \'Monster\', admin_dob = \'2021-01-29\' WHERE admin_hashedid = \'9z$Cw)J@NcRfUjX\';";
 
-static final String updateAdminUsername = "UPDATE ADMINISTRATOR SET admin_username = 'Supreme Being' WHERE admin_hashedid = '9z$Cw)J@NcRfUjX';";
+static final String updateAdminUsername = "UPDATE ADMINISTRATOR SET admin_username = \'Supreme Being\' WHERE admin_hashedid = \'9z$Cw)J@NcRfUjX\';";
 
-static final String updateAdminName = "UPDATE ADMINISTRATOR SET admin_fname = 'All', admin_lname = 'Father' WHERE admin_hashedid = '9z$Cw)J@NcRfUjX';";
+static final String updateAdminName = "UPDATE ADMINISTRATOR SET admin_fname = \'All\', admin_lname = \'Father\' WHERE admin_hashedid = \'9z$Cw)J@NcRfUjX\';";
 
-static final String viewAdminEmpRow = "SELECT * FROM ADMINISTRATOR WHERE admin_hashedid = '9z$Cw)J@NcRfUjX';";
+static final String viewAdminEmpRow = "SELECT * FROM ADMINISTRATOR WHERE admin_hashedid = \'9z$Cw)J@NcRfUjX\';";
 
-static final String deleteAdminAccount = "DELETE FROM ADMINISTRATOR WHERE admin_hashedid = '9z$Cw)J@NcRfUjX';";
+static final String deleteAdminAccount = "DELETE FROM ADMINISTRATOR WHERE admin_hashedid = \'9z$Cw)J@NcRfUjX\';";
 
 }
